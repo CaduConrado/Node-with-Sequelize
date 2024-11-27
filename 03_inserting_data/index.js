8a9752cae11c247b7ad4867b36d711c8c1e52f44
CaduConrado <caducngo@gmail.com>
2024-11-27 13:42:27 -0300

    Adding relations at database

--- 03_inserting_data/index.js
+++ 03_inserting_data/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const conn = require("./db/conn");
 const Officer = require("./models/Officer");
+const Patron = require("./models/Patron");
+const Arma = require("./models/Arma");
 const app = express();
 const port = 3000;
 
@@ -38,6 +40,24 @@ app.get("/officer/create", (req, res) => {
   res.render("addOfficer");
 });
 
+app.get("/officer/edit/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const officer = await Officer.findOne({ raw: true, where: { id: id } });
+
+  res.render("officerEdit", { officer });
+});
+
+app.get("/patron/showAll", async (req, res) => {
+  const patrons = await Patron.findAll({ raw: true });
+  res.status(200).json(patrons);
+});
+
+app.get("/arma/showAll", async (req, res) => {
+  const armas = await Arma.findAll({ raw: true });
+  res.status(200).json(armas);
+});
+
 app.post("/officer/create", async (req, res) => {
   const name = req.body.name;
   const rank = req.body.rank;
@@ -58,14 +78,6 @@ app.post("/officer/delete/:id", async (req, res) => {
 
   await Officer.destroy({ where: { id: id } });
   res.redirect("/officer/showAll");
-});
-
-app.get("/officer/edit/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const officer = await Officer.findOne({ raw: true, where: { id: id } });
-
-  res.render("officerEdit", { officer });
 });
 
 app.post("/officer/update", (req, res) => {
@@ -89,6 +101,34 @@ app.post("/officer/update", (req, res) => {
   Officer.update(data, { where: { id: id } });
   res.redirect("/officer/showAll");
 });
+
+app.post("/patron/create", async (req, res) => {
+  const name = req.body.name;
+  const rank = req.body.rank;
+
+  await Patron.create({ name, rank });
+  res.status(200).json("CRIADO COM SUCESSO");
+});
+
+app.post("/arma/create", async (req, res) => {
+  const arma = req.body.arma;
+  const patron = req.body.name;
+  console.log(patron);
+  const PatronId = await getPatronId(patron);
+  const body = {
+    arma,
+    PatronId,
+  };
+  await Arma.create(body);
+  res.status(200).json("CRIADO COM SUCESSO");
+});
+
+async function getPatronId(name) {
+  const patron = await Patron.findOne({ raw: true, where: { name: name } });
+  console.log(typeof patron.id);
+
+  return patron.id;
+}
 //condiciono a sincronização para a aplicação funcionar
 //a aplicação não funciona sem que as tabelas sejam criadas
 conn.sync().then(() => {
